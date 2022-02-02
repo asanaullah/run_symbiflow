7b1b89753b44c00b2438dffad0ab93563a9d958c
Ahmed <ahmedsanaullah.1@gmail.com>
2022-02-02 10:26:14 -0500

    gcc 4.8 fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export CXX="g++ -std=c++03"
-cd gcc-4.8.2
-./contrib/download_prerequisites
-cd ..
+
 mkdir gcc_build
 cd gcc_build
 ${PWD}/../gcc-4.8.2/configure --prefix=${PWD}/../gcc --enable-languages=c,c++  --disable-multilib --disable-libsanitizer
